aec44ed2aec7fe346cfda9c6843f6263ed91458f
Nicolas Graves <ngraves@ngraves.fr>
2025-02-24 09:55:26 +0100

    local: Add make-local-build-system and build-in-local-container

---
+++ src/guix-stack/build/local-build-system.scm
@@ -0,0 +1,88 @@
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; Copyright © 2025 Nicolas Graves <ngraves@ngraves.fr>
+
+(define-module (guix-stack build local-build-system)
+  #:use-module (guix gexp)
+  #:use-module (guix channels)
+  #:use-module (guix derivations)
+  #:use-module (guix packages)
+  #:use-module (guix profiles)
+  #:use-module (guix store)
+  #:use-module (guix monads)
+  #:use-module (guix scripts environment)
+  #:use-module (gnu system file-systems)
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 match)
+  #:use-module (gnu packages)
+  #:use-module (guix build utils)
+  #:use-module (guix build-system)
+  #:export (make-local-build-system
+            build-in-local-container))
+
+(define (make-local-lower old-lower target-directory modules)
+  (lambda* args
+    (let ((old-bag (apply old-lower args)))
+      (bag
+        (inherit old-bag)
+        (build
+         (lambda* (name inputs #:key (outputs '("out"))
+                        #:allow-other-keys #:rest rest)
+           (mlet %store-monad
+               ((builder (apply (bag-build old-bag)
+                                name inputs #:outputs outputs rest)))
+             (return
+              #~(begin
+                  (use-modules #$@modules)
+                  (with-directory-excursion #$target-directory
+                    (for-each
+                     (lambda (out)
+                       (setenv
+                        out (string-append #$target-directory "/" out)))
+                     '#$outputs)
+                    #$builder))))))))))
+
+(define* (make-local-build-system target-build-system
+                                  #:key
+                                  (target-directory (getcwd))
+                                  (modules '((guix build utils))))
+  (build-system
+    (name (symbol-append
+           (build-system-name target-build-system) '-local))
+    (description (string-append
+                  (build-system-description target-build-system)
+                  " ; applied as current user in " target-directory))
+    (lower (make-local-lower (build-system-lower target-build-system)
+                             target-directory modules))))
+
+(define* (build-in-local-container store package)
+  "Build local PACKAGE in a container locally."
+  (with-store store
+    ;; We can't use package->derivation directly because we want the
+    ;; user rather than the daemon to build the derivation.
+    ;; This allows us to have access to the pre-built files without
+    ;; having to mess with hashes or timestamps.
+    (let* ((manifest (package->development-manifest package))
+           (bag (package->bag package))
+           ;; See (@@ (guix scripts environment) manifest->derivation).
+           (prof-drv ((store-lower profile-derivation)
+                      store manifest #:allow-collisions? #t))
+           (drv ((@@ (guix packages) bag->derivation*) store bag package))
+           (_ (build-derivations store
+                                 (cons* prof-drv (derivation-inputs drv))))
+           (profile (derivation->output-path prof-drv)))
+      (catch #t
+        (lambda ()
+          ((store-lower launch-environment/container)
+           store
+           #:command (cons* (derivation-builder drv)
+                            (derivation-builder-arguments drv))
+           #:bash (string-append profile "/bin/bash")
+           #:map-cwd? #t
+           #:user-mappings
+           (list (specification->file-system-mapping "/gnu/store" #f))
+           #:profile profile
+           #:manifest manifest))
+        (lambda args
+          (match args
+            (('quit 0) #t)
+            (_         #f)))))))
