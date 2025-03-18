2d96a96a87f4a4c98d733943318ac6954bd3b7ad
Nicolas Graves <ngraves@ngraves.fr>
2025-03-19 00:43:36 +0100

    pin package

--- .guix/modules/guix-stack-channel.scm
+++ .guix/modules/guix-stack-channel.scm
@@ -35,8 +35,8 @@
                   "0jnq2snzq0zj2rmbfgzvxnzf4swfkj6air54ipxl6hyaxbli1873")))))))
 
 (define-public guix-stack
-  (let ((commit "3100925")
-        (revision "88"))
+  (let ((commit "2681f86")
+        (revision "89"))
     (package
       (name "guix-stack")
       (version (git-version "0.0.0" revision commit))
