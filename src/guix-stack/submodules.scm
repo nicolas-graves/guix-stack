ca2c9266f3bfb8ae370d29f25acebc5f56866cce
Nicolas Graves <ngraves@ngraves.fr>
2025-04-10 01:15:53 +0200

    Add complex safe version of submodules-dir->channels.
    This version goes through submodules, and find the parent
    repository...
    
    Featureful, but we don't really need that complexity especially later
    when we might have dozens of channels and packages we keep an eye on.

--- src/guix-stack/submodules.scm
+++ src/guix-stack/submodules.scm
@@ -8,7 +8,31 @@
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-1)
   #:use-module (git)
-  #:export (submodules-dir->packages))
+  #:export (submodules-dir->channels
+            submodules-dir->packages))
+
+(define* (submodules-dir->channels #:optional (dir (getcwd))
+                                   #:key
+                                   (parent-dir
+                                    (dirname (repository-discover dir))))
+  "Return generated <channel>s from DIR.
+
+DIR is assumed to be a directory where all subdirectories are submodules."
+  (let ((this-repo (repository-open parent-dir))
+        (relative-dir (string-drop (canonicalize-path dir)
+                                   (1+ (string-length parent-dir)))))
+    (filter-map
+     (match-lambda
+       ((? (cut string-prefix? relative-dir <>) path)
+        (let ((this-sub (submodule-lookup this-repo path)))
+          (channel
+           (name (string->symbol (basename path)))
+           (branch (submodule-branch this-sub))
+           (commit (oid->string (submodule-head-id this-sub)))
+           (url (submodule-url this-sub)))))
+       ;; Otherwise: other submodules.
+       (_ #f))
+     (repository-submodules this-repo))))
 
 (define* (submodules-dir->packages #:optional (dir (getcwd))
                                    #:key
