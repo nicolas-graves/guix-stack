08906b2762e091dcdd55a5aaa993d414a53ab460
Nicolas Graves <ngraves@ngraves.fr>
2025-04-26 12:54:07 +0200

    guix: try removing some transformation

--- src/guix-stack/build/channel.scm
+++ src/guix-stack/build/channel.scm
@@ -140,10 +140,10 @@ This enables us not to try and run build steps when not necessary."
            (arguments
             (substitute-keyword-arguments (package-arguments local-guix)
               ;; Disable translations for speed.
-              ((#:configure-flags flags #~'())
-               #~(cons* "--disable-nls" #$flags))
-              ((#:modules modules)
-               `((srfi srfi-26) ,@modules))
+              ;; ((#:configure-flags flags #~'())
+               ;; #~(cons* "--disable-nls" #$flags))
+              ;; ((#:modules modules)
+               ;; `((srfi srfi-26) ,@modules))
               ((#:phases phases #~%standard-phases)
                #~(modify-phases #$phases
                    ;; Disable translations for speed.
