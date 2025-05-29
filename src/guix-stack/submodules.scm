a42bdc82c4944ce0263a4f7b8ed6381047f59f0f
Nicolas Graves <ngraves@ngraves.fr>
2025-05-29 14:56:47 +0200

    submodules: Split channel-submodules.
    This is to allow loading the submodules-dir->channels without pulling
    in guix-local.

--- src/guix-stack/submodules.scm
+++ src/guix-stack/submodules.scm
@@ -3,43 +3,13 @@
 
 (define-module (guix-stack submodules)
   #:use-module (gnu packages)
-  #:use-module (guix channels)
   #:use-module (guix-local source)
   #:use-module (ice-9 ftw)
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-1)
-  #:use-module (srfi srfi-2)
-  #:use-module (git)
-  #:export (submodules-dir->channels
-            submodules-dir->packages))
-
-(define* (submodules-dir->channels #:optional (dir (getcwd))
-                                   #:key
-                                   (parent-dir
-                                    (dirname (repository-discover dir)))
-                                   (use-local-urls? #f))
-  "Return generated <channel>s from DIR.
-
-DIR is assumed to be a directory where all subdirectories are submodules."
-  (let* ((this-repo (repository-open parent-dir))
-         (relative-dir (if (string-prefix? parent-dir dir)
-                           (string-drop dir (1+ (string-length parent-dir)))
-                           dir)))
-    (filter-map
-     (match-lambda
-       ((or "." "..") #f)
-       (path
-        (and-let* ((this-sub (submodule-lookup
-                              this-repo
-                              (string-append relative-dir "/" path))))
-          (channel
-           (name (string->symbol (basename path)))
-           (branch (submodule-branch this-sub))
-           (commit (oid->string (submodule-wd-id this-sub)))
-           (url (if use-local-urls?
-                    (string-append (canonicalize-path dir) "/" path)
-                    (submodule-url this-sub)))))))
-     (scandir dir))))
+  #:use-module (guix-stack channel-submodules)
+  #:export (submodules-dir->packages)
+  #:re-export (submodules-dir->channels))
 
 (define* (submodules-dir->packages #:optional (dir (getcwd))
                                    #:key
