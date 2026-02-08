6ae3d23b90ca5761b51ecb5bf22d1e4cb3ba9d65
Nicolas Graves <ngraves@ngraves.fr>
2026-02-09 00:26:58 +0100

    channel-submodules: Various improvements.
    Rationale: Improve it enough to be able to generate what's needed to
    create a valid channel list from configuration.scm.
    
    * guix-stack/channel-submodules.scm:
    (get-submodule-oid): Add procedure.
    (submodules-dir->channels): Improve procedure, add type keyword.

--- src/guix-stack/channel-submodules.scm
+++ src/guix-stack/channel-submodules.scm
@@ -2,48 +2,72 @@
 ;; Copyright © 2025 Nicolas Graves <ngraves@ngraves.fr>
 
 (define-module (guix-stack channel-submodules)
+  #:use-module (guix build utils)
   #:use-module (guix channels)
+  #:use-module (guix git)
   #:use-module (ice-9 ftw)
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-2)
+  #:use-module (srfi srfi-26)
   #:use-module (git)
   #:export (submodules-dir->channels))
+
+(define (get-submodule-oid full-path branch)
+  "Get the OID of BRANCH for a submodule by opening its path."
+  ;; XXX: This could techniocally be improved by using submodule_open
+  ;; but it's not implemented yet.
+  (with-repository full-path submodule-repository
+    (and=> (false-if-exception
+            (branch-lookup submodule-repository branch BRANCH-REMOTE))
+           reference-target)))
 
 (define* (submodules-dir->channels #:optional (dir (getcwd))
                                    #:key
                                    (parent-dir
                                     (dirname (repository-discover dir)))
-                                   (use-local-urls? #f))
+                                   (use-local-urls? #f)
+                                   (type '(head)))
   "Return generated <channel>s from DIR.
 
 DIR is assumed to be a directory where all subdirectories are submodules."
-  (let* ((this-repo (repository-open parent-dir))
-         (relative-dir (if (string-prefix? parent-dir dir)
-                           (string-drop dir (1+ (string-length parent-dir)))
-                           dir)))
-    (filter-map
-     (match-lambda
-       ((or "." "..") #f)
-       (path
-        (and-let* ((this-sub (catch 'git-error
-                               (lambda ()
-                                 (submodule-lookup
-                                  this-repo
-                                  (string-append relative-dir "/" path)))
-                               (lambda (key error . rest)
-                                 (if (= GIT_EEXISTS (git-error-code error))
-                                     (begin
-                                       (format (current-error-port) "\
+  (with-repository parent-dir this-repo
+    (let* ((relative-dir (if (and (> (string-length parent-dir)
+                                     (string-length dir))
+                                  (string-prefix? parent-dir dir))
+                             (string-drop dir (1+ (string-length parent-dir)))
+                             dir)))
+      (filter-map
+       (match-lambda
+         ((or "." "..") #f)
+         ((? (compose directory-exists?
+                      (cut string-append relative-dir "/" <>))
+             path)
+          (and-let*
+              ((full-path (string-append relative-dir "/" path))
+               (this-sub (catch 'git-error
+                                 (lambda ()
+                                   (submodule-lookup this-repo full-path))
+                                 (lambda (key error . rest)
+                                   (if (= GIT_EEXISTS (git-error-code error))
+                                       (begin
+                                         (format (current-error-port) "\
 git-error: check that every submodule has its branch set in .gitmodules.~%")
-                                       #f)
-                                     (apply throw key error rest)))))
-                   (oid (submodule-wd-id this-sub)))
-          (channel
-           (name (string->symbol (basename path)))
-           (branch (submodule-branch this-sub))
-           (commit (oid->string oid))
-           (url (if use-local-urls?
-                    (string-append (canonicalize-path dir) "/" path)
-                    (submodule-url this-sub)))))))
-     (scandir dir))))
+                                         #f)
+                                       (apply throw key error rest)))))
+               (oid (match type
+                      (('head)
+                       (submodule-wd-id this-sub))
+                      (('branch 'or branches ...)
+                       (any (cut get-submodule-oid full-path <>) branches))
+                      (`(branch . ,branch)
+                       (get-submodule-oid full-path branch)))))
+            (channel
+              (name (string->symbol (basename path)))
+              (branch (submodule-branch this-sub))
+              (commit (oid->string oid))
+              (url (if use-local-urls?
+                       (string-append (canonicalize-path dir) "/" path)
+                       (submodule-url this-sub))))))
+         (_ #f))
+       (scandir dir)))))
